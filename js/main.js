66708d7f1ebe7056b3d765dc378ad57fbbf4359f
YukaNagatomo <chatsumi.yuchan@gmail.com>
2019-06-18 04:42:54 +0900

    add work

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 $(function () {
-    //    $(window).resize(function () {
-    //        location.reload();
-    //    })
-    //    modaal.js
-    $('.modal').modaal({
-        background: '#000',
-        overlay_opacity: '0.3'
-    });
-    //    スムーススクロール
-    $('a[href^="#link-"]').click(function () {
-        var speed = 500;
-        var href = $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
-        $("html, body").animate({
-            scrollTop: position
-        }, speed, "swing");
-        return false;
-    });
-    //    animate.css と jquery.animateCssPlus.js
-    $('.fadein').animateCssPlus({
-        scroll: true,
-        class: 'fadeInUp'
-    });
-
+  //    $(window).resize(function () {
+  //        location.reload();
+  //    })
+  //    modaal.js
+  $('.modal').modaal({
+    background: '#000',
+    overlay_opacity: '0.3'
+  });
+  //    スムーススクロール
+  $('a[href^="#link-"]').click(function () {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+  $('.work').on('inview', function (event, isInView) {
+    if (isInView) {
+      //表示領域に入った時
+      $(this).addClass('fadeInUp');
+    } 
+  });
 });
